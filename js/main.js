143c4f23595cf6206050d371dd61015d4f9c3a0e
meekyung <meekyung09111@gmail.com>
2025-11-25 02:09:54 +0900

    feat: 슬라이드

--- js/main.js
+++ js/main.js
@@ -1,4 +1,36 @@
 const btt = document.querySelector('#btt');
+const testimonial = document.querySelector('.testimonial');
+const testimonialUl = testimonial.querySelector('.testimonial ul');
+const testimonialUlBlockquote = testimonialUl.querySelector('.testimonial ul blockquote');
+let currentIdx = 0;
+const prevBtn = testimonial.querySelector('.prev');
+const nextBtn = testimonial.querySelector('.next');
+const blockquoteCount = blockquote.lingth;
+
+blockquote.forEach((item,i)=>{
+  item.style.left = i*100+'%';
+})
+
+moveSlide(0);
+
+function moveSlide(idx){
+  testimonialUl.style.left = -idx*100+'%';
+  currentIdx = idx;
+  for(let blockquote of blockquote){
+    blockquote.classList.remove('active');
+  }
+  blockquote[idx].classList.add('active');
+}
+
+nextBtn.addEventListener('click',()=>{
+  let nextIdx = (currentIdx + 1)%blockquoteCount;
+    moveSlide(nextIdx);
+});
+prevBtn.addEventListener('click',()=>{
+  let nextIdx = (currentIdx - 1 + 1)%blockquoteCount;
+    moveSlide(nextIdx);
+});
+
 window.scrollTo(0, 100);
 window.addEventListener('scroll',()=>{
   let scrAmt = window.scrollY;
